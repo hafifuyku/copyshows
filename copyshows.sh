f70a0926d3b80a3acd5614a8c96aeef652f34e76
Hasan Yalçınkaya <hasan@pilli.com>
2024-04-22 14:19:36 +0100

    Update copyshows.sh
    Show episode number when moving
    Always title case tv show folders
    Strict season folders of the format season xx

--- copyshows.sh
+++ copyshows.sh
@@ -11,15 +11,18 @@ for item in "$source_directory"/*; do
     if [ -f "$item" ] || [ -d "$item" ]; then
         echo "Processing:"
 
-        # Extract the TV show name and season number from the file or folder name
+        # Extract the TV show name, season number, and episode number from the file or folder name
         filename=$(basename "$item")
         tv_show_name=$(echo "$filename" | sed -E 's/(.*[.])?[Ss]([0-9]+)[Ee]([0-9]+).*/\1/' | tr '.' ' ')
-        season_number=$(echo "$filename" | sed -E 's/(.*[.])?[Ss]([0-9]+)[Ee]([0-9]+).*/\2/')
+        season_number=$(echo "$filename" | sed -E 's/.*[Ss]([0-9]+)[Ee][0-9]+.*/\1/')
+        episode_number=$(echo "$filename" | sed -E 's/.*[Ss][0-9]+[Ee]([0-9]+).*/\1/')
 
         tv_show_name=${tv_show_name%" "}  # Remove trailing space from show name
+        tv_show_name=$(echo "$tv_show_name" | awk '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2));}1') # Convert show name to title case
 
         echo "TV Show Name: $tv_show_name"
         echo "Season Number: $season_number"
+        echo "Episode Number: $episode_number"
 
         # Check if the TV show folder exists in the destination directory (ignoring case sensitivity)
         show_folder=""
@@ -38,7 +41,6 @@ for item in "$source_directory"/*; do
         # If the TV show folder doesn't exist, create it
         if [ -z "$show_folder" ]; then
             show_folder="$destination_directory/$tv_show_name"
-            show_folder=${show_folder//./ }  # Replace dots with spaces
             mkdir "$show_folder"
             echo "Created show folder: $show_folder"
         fi
@@ -52,6 +54,6 @@ for item in "$source_directory"/*; do
 
         # Move the file or folder to the season folder
         mv "$item" "$season_folder"
-        echo "Moved to $season_folder"
+        echo "Moved episode $episode_number to $season_folder"
     fi
 done
